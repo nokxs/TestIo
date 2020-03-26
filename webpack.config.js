11ad8b9e9e6aa70751fe8987076e3abccd09ec6d
Niklas <niklas@nokxs.de>
2020-03-26 15:42:52 +0100

    webpack with ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,18 @@
 module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
   entry: "./src/index.ts",
   output: {
-    filename: "./dist/bundle.js",
+    filename: "bundle.js",
   },
-
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
-
   resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"],
   },
-
   module: {
-    loaders: [
-      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-    ],
-
-    preLoaders: [
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { test: /\.js$/, loader: "source-map-loader" },
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: "ts-loader" },
     ],
   },
 };
